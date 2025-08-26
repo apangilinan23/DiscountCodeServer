34865c112d61649b204c5f4e6b830de3f5c43778
apangilinan <pangilinan_adrian21@yahoo.com>
2025-08-26 11:14:27 +0800

    Add project files.

---
+++ DiscountCodeServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var ip = IPAddress.Any;
+using (var server = new TcpListener(ip, 8082))
+{
+    server.Start();
+    Console.WriteLine("Server started. Waiting for connections...");
+    while (true)
+    {
+        using (var client = server.AcceptTcpClient())
+        {
+            Console.WriteLine("Client connected!");
+
+            using (var stream = client.GetStream())
+            {
+                var buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Received data: {receivedData}");
+
+                string response = "Hello from server!";
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                stream.Write(responseBytes, 0, responseBytes.Length);
+
+                Console.WriteLine("Waiting for connections...");
+            }
+        }
+    }
+    
+}
